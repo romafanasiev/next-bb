e03f83f6cecb53578b5ba46e6b9631afac5429e2
Roman Afanasiev <roman.afanasiev@chisw.com>
2023-04-24 16:39:16 +0300

    fixing admin routes

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,15 +1,26 @@
-import { withAuthUser, AuthAction, withAuthUserSSR } from 'next-firebase-auth';
+import { useAuthUser, withAuthUser } from 'next-firebase-auth';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
-import { protectedPages } from 'helpers';
+const Admin = () => {
+  const router = useRouter();
+  const user = useAuthUser();
 
-const Admin = () => (
-  <main className="bg-slate-700 p-10">
-    <p>Admin page</p>
-  </main>
-);
+  useEffect(() => {
+    if (!user.claims.admin) {
+      router.push('/');
+    }
+  }, [user]);
 
-export const getServerSideProps = withAuthUserSSR({
-  whenUnauthed: AuthAction.REDIRECT_TO_LOGIN,
-})(async ({ AuthUser }) => await protectedPages(AuthUser));
+  return (
+    <main className="bg-slate-700 p-10">
+      <p>Admin page</p>
+    </main>
+  );
+};
+
+// export const getServerSideProps = withAuthUserSSR({
+//   whenUnauthed: AuthAction.REDIRECT_TO_LOGIN,
+// })(async ({ AuthUser }) => await protectedPages(AuthUser));
 
 export default withAuthUser()(Admin);
